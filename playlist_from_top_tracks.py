d9e5bd8d9d85a110e7613c29df381861cfc12b0a
bren <gamingajun10@icloud.com>
2022-12-30 19:58:30 -0500

    fix frontend

--- playlist_from_top_tracks.py
+++ playlist_from_top_tracks.py
@@ -1,6 +1,7 @@
 import datetime
 import api
 import time
+import sys
 
 print("Welcome.")
 limit = int(input("How many top tracks to get?: "))
@@ -19,20 +20,24 @@ playlist = []
 for i, track in enumerate(out):
     if i == limit:
         break
-    print(f"Getting {track}...")
-    
-    rawTrack = api.spGetTrack(auth, track, out[track]["artist"], out[track]["album"])
-    print(rawTrack)
-    playlist.append(rawTrack)
+    print(f"\nGetting {track}", end='\r')
+    rawTrack = api.spGetTrack(auth, track, out[track]["artist"])
+    if rawTrack is None:
+        print(f"\n\033[1mCouldn't find {track} by {out[track]['artist']}. Consider adding it manually.\033[0m", end='\r')
+        time.sleep(0.5)
+        continue
+    else:
+        print(f"Matched {track} to {rawTrack[1]} by {rawTrack[2]}", end='\r')
+        playlist.append(rawTrack[0])
 
 print(playlist)
 
 playlistData = []
 playlistData.append(input("Playlist Name: "))
-if "y" in input("Public?").lower():
+if "y" in input("Public? (y/n):").lower():
     playlistData.append("true")
 else:
     playlistData.append("false")
 
-playlistID = spCreatePlaylist(auth, playlistData)
-print(spAddToPlaylist(auth, playlistID, playlist))
+playlistID = api.spCreatePlaylist(auth, playlistData)
+print(api.spAddToPlaylist(auth, playlistID, playlist))
